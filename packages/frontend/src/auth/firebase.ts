9c297728d3117e918e78347ee2c8c1fa7598f787
Maggie Pedersen <mped822@aucklanduni.ac.nz>
2022-03-22 09:33:03 +1300

    Rename env variable

--- packages/frontend/src/auth/firebase.ts
+++ packages/frontend/src/auth/firebase.ts
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = JSON.parse(
-  process.env.REACT_APP_FIREBASE_CONFIG ?? '{}');
+  process.env.FIREBASE_CONFIG_JSON ?? '{}');
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Use Firebase auth
+// Use this to utilise Firebase auth
 export const auth = getAuth(app);
