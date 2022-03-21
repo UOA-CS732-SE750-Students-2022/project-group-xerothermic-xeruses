02d19987881896b22188f65f6777703eca7e4ee7
Matt Moran <mattmoran2000@gmail.com>
2022-03-21 15:32:05 +1300

    Enable jest resolving path aliases from tsconfig

---
+++ packages/backend/jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from '@jest/types';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config.InitialOptions = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+
+  // Enable resolving path aliases from tsconfig.json.
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: __dirname }),
+};
+
+export default config;
