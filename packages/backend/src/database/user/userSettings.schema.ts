c11a3d83fd22ec4b0b45fc1659eebda8122d8e06
Matt Moran <mattmoran2000@gmail.com>
2022-03-22 14:04:08 +1300

    Convert UserSettingsTheme to an enum

--- packages/backend/src/database/user/userSettings.schema.ts
+++ packages/backend/src/database/user/userSettings.schema.ts
@@ -4,7 +4,11 @@ import { type Document, type Types } from 'mongoose';
 /**
  * UserSettingsTheme specifies the theming for the web interface.
  */
-export type UserSettingsTheme = 'light' | 'dark' | 'system';
+export enum UserSettingsTheme {
+  DARK = 'dark',
+  LIGHT = 'light',
+  SYSTEM = 'system',
+}
 
 /**
  * UserSettings is an embedded document containing preferences for a single user.
