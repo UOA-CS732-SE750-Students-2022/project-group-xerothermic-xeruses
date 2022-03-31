10e8f16c6c37d45820b8d1306a04e69f32c6ca79
beverleysun <beverleysun0205@gmail.com>
2022-03-31 20:08:46 +1300

    Use SERVER_URI instead of SERVER_BASE_URI

--- packages/frontend/src/apollo/index.ts
+++ packages/frontend/src/apollo/index.ts
@@ -1,11 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const SERVER_BASE_URI = process.env.REACT_APP_SERVER_BASE_URI;
-if (!SERVER_BASE_URI) {
-  throw new ReferenceError('REACT_APP_SERVER_BASE_URI is not defined.');
+const SERVER_URI = process.env.REACT_APP_SERVER_URI;
+if (!SERVER_URI) {
+  throw new ReferenceError('REACT_APP_SERVER_URI is not defined.');
 }
-
-const SERVER_URI = `${SERVER_BASE_URI}/graphql`;
 
 const client = new ApolloClient({
   uri: SERVER_URI,
