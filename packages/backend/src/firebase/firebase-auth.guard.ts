d7bfb9b5377d8269d117d4a90e145463497dfa66
Maggie Pedersen <mped822@aucklanduni.ac.nz>
2022-04-03 22:31:13 +1200

    Add return types

--- packages/backend/src/firebase/firebase-auth.guard.ts
+++ packages/backend/src/firebase/firebase-auth.guard.ts
@@ -1,11 +1,12 @@
+import { IncomingMessage } from 'http';
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class FirebaseAuthGuard extends AuthGuard('firebase-auth') {
-  getRequest(executionContext: ExecutionContext) {
-    const ctx = GqlExecutionContext.create(executionContext);
-    return ctx.getContext().req;
+  getRequest(executionContext: ExecutionContext): IncomingMessage {
+    const context = GqlExecutionContext.create(executionContext);
+    return context.getContext().req;
   }
 }
