32090cf648f68aa69339e8ec753cbb42f7c4f5c0
Matt Moran <mattmoran2000@gmail.com>
2022-04-13 23:32:47 +1200

    Fix settings was present on DTO but is unused

--- packages/api-types/src/mutations/addUserInput.ts
+++ packages/api-types/src/mutations/addUserInput.ts
@@ -1,10 +1,8 @@
 import { UserDTO } from '../user';
-import { UserSettingsDTO } from '../userSettings';
 
 /**
  * Create a User.
  */
 export interface AddUserInputDTO extends Partial<UserDTO> {
   name: string;
-  settings?: UserSettingsDTO;
 }
