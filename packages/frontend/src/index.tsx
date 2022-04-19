c3f762408a93462a309c8728c03b7630172e7eb2
alieevee <yshe735@aucklanduni.ac.nz>
2022-04-20 01:09:58 +1200

    Use StyledEngineProvider to control CSS injection

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import apolloClient from './apollo';
 import { ApolloProvider } from '@apollo/client';
+import { StyledEngineProvider } from '@mui/material/styles';
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={apolloClient}>
-      <App />
+      <StyledEngineProvider injectFirst>
+        <App />
+      </StyledEngineProvider>
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root'),
