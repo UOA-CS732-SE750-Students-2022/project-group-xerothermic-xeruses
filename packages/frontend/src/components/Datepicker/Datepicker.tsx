71e121a6b0b6cb5b2637613dfcd3fbec56f307fa
alieevee <yshe735@aucklanduni.ac.nz>
2022-04-23 19:56:57 +1200

    Add callback function

--- packages/frontend/src/components/Datepicker/Datepicker.tsx
+++ packages/frontend/src/components/Datepicker/Datepicker.tsx
@@ -3,12 +3,16 @@ import styles from 'react-day-picker/dist/style.module.css';
 import customStyles from './Datepicker.module.css';
 import { DayPicker } from 'react-day-picker';
 
-const Datepicker = () => {
+const Datepicker = (datesPicked: (dates: Date[]) => Date[] | undefined) => {
   const initialDays: Date[] = [];
   const [days, setDays] = useState<Date[] | undefined>(initialDays);
 
   const footer =
     days && days.length > 0 ? <p>You selected {days.length} day(s).</p> : <p>Please pick one or more days.</p>;
+
+  datesPicked = (dates: Date[]) => {
+    return days;
+  };
 
   return (
     <DayPicker
