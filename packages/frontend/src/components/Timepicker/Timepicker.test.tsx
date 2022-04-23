2a3fcaa3f9dd0ebee746cd32ad334e858b2dbb92
alieevee <yshe735@aucklanduni.ac.nz>
2022-04-23 19:46:44 +1200

    Reformat test

--- packages/frontend/src/components/Timepicker/Timepicker.test.tsx
+++ packages/frontend/src/components/Timepicker/Timepicker.test.tsx
@@ -16,10 +16,7 @@ it('should show label', () => {
 it('should select the right time option when clicked', () => {
   render(<Timepicker label="timepicker" />);
   fireEvent.mouseDown(screen.getByRole('button'));
-
   const listbox = within(screen.getByRole('listbox'));
-
   fireEvent.click(listbox.getByText(/9 AM/i));
-
   expect(screen.getByRole('menu')).toHaveTextContent(/9 AM/i);
 });
