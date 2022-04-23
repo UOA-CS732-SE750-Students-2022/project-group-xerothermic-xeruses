b25e63a27bbe2a0c446b87af78ebe90cedd60a42
alieevee <yshe735@aucklanduni.ac.nz>
2022-04-23 22:34:04 +1200

    Add test

--- packages/frontend/src/components/Datepicker/Datepicker.test.tsx
+++ packages/frontend/src/components/Datepicker/Datepicker.test.tsx
@@ -1,7 +1,32 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Datepicker from './Datepicker';
 
-test('first test', () => {
+it('should render', () => {
+  const { container } = render(<Datepicker />);
+  expect(container).toBeVisible();
+});
+
+it('should prompt user to select a day when none has been selected', () => {
   render(<Datepicker />);
+  const pleaseSelectMessage = screen.getAllByText(/Please pick/i);
+  expect(pleaseSelectMessage).toHaveLength(1);
+});
+
+it('should be able to select a day', () => {
+  render(<Datepicker />);
+  const buttons = screen.getAllByRole('button');
+  fireEvent.click(buttons[2]);
+  const selectedMessage = screen.getAllByText(/You selected/i);
+  expect(selectedMessage).toHaveLength(1);
+});
+
+it('should be able to select multiple days', () => {
+  render(<Datepicker />);
+  const buttons = screen.getAllByRole('button');
+  fireEvent.click(buttons[2]);
+  fireEvent.click(buttons[3]);
+  const selectedMessage = screen.getAllByText(/2 day/i);
+  expect(selectedMessage).toHaveLength(1);
 });
