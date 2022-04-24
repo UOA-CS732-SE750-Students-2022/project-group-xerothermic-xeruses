0878aaf1712acbff441c6eceb6e7f991533f2aaa
Maggie Pedersen <mped822@aucklanduni.ac.nz>
2022-04-24 22:21:56 +1200

    Remove unused dependency

--- packages/backend/src/firebase/validate-firebase-auth.strategy.ts
+++ packages/backend/src/firebase/validate-firebase-auth.strategy.ts
@@ -1,14 +1,12 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 // eslint-disable-next-line import/no-unresolved
 import { DecodedIdToken, getAuth } from 'firebase-admin/auth';
 import { ExtractJwt, Strategy } from 'passport-firebase-jwt';
-import { FirebaseConfig } from '~/config/firebaseConfig.schema';
 
 @Injectable()
 export class ValidateFirebaseAuthStrategy extends PassportStrategy(Strategy, 'validate-firebase-auth') {
-  constructor(private readonly configService: ConfigService<FirebaseConfig, true>) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
