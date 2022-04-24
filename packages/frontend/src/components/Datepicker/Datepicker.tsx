4a18c0d875ddb591f63cf2fa6e6cfaff62d210aa
yshe735 <yshe735@aucklanduni.ac.nz>
2022-04-24 21:42:54 +1200

    Add Datepicker component (#84)
    * Add datepicker component
    
    * Remove unused prop
    
    * Add callback function
    
    * Add test
    
    * Add prop types
    
    * Update datepicker component
    
    * Change datesPicked function return type
    
    * Save firebaseID when creating a user (#68)
    
    * Save firebaseID when creating a user
    
    * Revert to _id
    
    * Update documents
    
    * Use shorter version:
    
    * Create flock with date and time range and generate meeting code (#69)
    
    * Create flock with date and time range and generate meeting code
    
    * Add missing files
    
    * Update test mocks
    
    * Make code unique:
    
    * Ask for flock days instead
    
    * Stop being dumb maggie
    
    * Regenerate flock code and move into service
    
    * Add user to flock via token and meeting code (#80)
    
    * Add timepicker component (#78)
    
    * Add timepicker component
    
    * Add callback function to get time
    
    * Update packages
    
    * Reformat test
    
    * Call timeChanged func and redo time logic
    
    * Add datesPicked callback function and order dates
    
    * Update test and clean up component
    
    * Add react-day-picker package
    
    * Wrap in div
    
    Co-authored-by: Maggie Pedersen <62322685+mped822@users.noreply.github.com>

---
+++ packages/frontend/src/components/Datepicker/Datepicker.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import styles from 'react-day-picker/dist/style.module.css';
+import customStyles from './Datepicker.module.css';
+import { DayPicker } from 'react-day-picker';
+
+type DatepickerProps = {
+  datesPicked: (dates: Date[]) => void;
+};
+
+const Datepicker = ({ datesPicked }: DatepickerProps) => {
+  const initialDays: Date[] = [];
+  const [days, setDays] = useState<Date[] | undefined>(initialDays);
+
+  const handleChange = (dates: Date[] | undefined) => {
+    setDays(dates);
+    if (dates) dates.sort((date1, date2) => date1.valueOf() - date2.valueOf());
+    datesPicked(dates as Date[]);
+  };
+
+  const footer =
+    days && days.length > 0 ? <p>You selected {days.length} day(s).</p> : <p>Please pick one or more days.</p>;
+
+  return (
+    <div>
+      <DayPicker
+        classNames={styles}
+        modifiersClassNames={{ selected: `${customStyles.selected}` }}
+        mode="multiple"
+        min={1}
+        selected={days}
+        onSelect={handleChange}
+        footer={footer}
+      />
+    </div>
+  );
+};
+
+export default Datepicker;
