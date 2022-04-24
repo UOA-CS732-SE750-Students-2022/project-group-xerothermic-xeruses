2f965162a5c481a194141e0e26b3372d4ec5b474
yshe735 <yshe735@aucklanduni.ac.nz>
2022-04-24 20:22:23 +1200

    Add timepicker component (#78)
    * Add timepicker component
    
    * Add callback function to get time
    
    * Update packages
    
    * Reformat test
    
    * Call timeChanged func and redo time logic

---
+++ packages/frontend/src/components/Timepicker/README.md
@@ -0,0 +1,10 @@
+# Timepicker
+
+A timepicker for choosing times by the hour based off of MUI Select component.
+
+## Props
+
+`label`
+
+- type: `string`
+- description: A label must be supplied to identify the purpose of the dropdown menu. Can be an empty string.
---
+++ packages/frontend/src/components/Timepicker/Timepicker.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import styles from './Timepicker.module.css';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+type TimepickerProps = {
+  label: string;
+  timeChanged: (date: Date) => void;
+};
+
+/* Generates all 24 hours in 12 hour format with AM/PM */
+function getHours() {
+  const hours = new Map<string, Date>();
+
+  for (let i = 0; i < 24; i++) {
+    const ampm = i >= 12 ? 'pm' : 'am';
+    const timeString = i > 12 ? `${i - 12}:00 ${ampm}` : `${i}:00 ${ampm}`;
+    const timeAsDate = new Date();
+    timeAsDate.setHours(i, 0, 0, 0);
+    hours.set(timeString, timeAsDate);
+  }
+
+  return hours;
+}
+
+const Timepicker = ({ label, timeChanged }: TimepickerProps) => {
+  const [time, setTime] = useState('');
+  const hours = getHours();
+
+  const handleChange = (event: SelectChangeEvent) => {
+    const timeString = event.target.value as string;
+    setTime(timeString);
+    timeChanged(hours.get(timeString) as Date);
+  };
+
+  return (
+    <div>
+      <FormControl variant="standard" className={styles.select}>
+        <InputLabel id="demo-simple-select-standard-label">{label}</InputLabel>
+        <Select
+          role="menu"
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value={time}
+          onChange={handleChange}
+        >
+          {Array.from(hours.keys()).map((hour) => (
+            <MenuItem role="menuitem" value={hour} key={hour}>
+              {hour}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+};
+
+export default Timepicker;
