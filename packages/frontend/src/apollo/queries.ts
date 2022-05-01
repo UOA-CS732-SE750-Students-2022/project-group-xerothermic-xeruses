7a94aa1db03c97ac8c51b60b51627e1b4b2ae9a4
alieevee <yshe735@aucklanduni.ac.nz>
2022-05-01 21:32:39 +1200

    Add query for getting user intervals

--- packages/frontend/src/apollo/queries.ts
+++ packages/frontend/src/apollo/queries.ts
@@ -1,5 +1,16 @@
 import { gql } from '@apollo/client';
 
 // Add queries in this file so that it doesn't clutter up our component files
+const GET_USER_INTERVALS = gql`
+  query UserIntervals($availabilityIds: [String!]!, $userIntervalInput: UserAvailabilityIntervalInput!) {
+    getUserIntervals(availabilityIds: $availabilityIds, userIntervalInput: $userIntervalInput) {
+      availability {
+        start
+        end
+        available
+      }
+    }
+  }
+`;
 
-export {};
+export { GET_USER_INTERVALS };
