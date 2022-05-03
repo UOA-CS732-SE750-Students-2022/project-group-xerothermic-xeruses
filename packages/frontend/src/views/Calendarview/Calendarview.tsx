95cb4519a8818ee9eb2642e4b9c4bfb8cc5ae203
alieevee <yshe735@aucklanduni.ac.nz>
2022-05-03 15:33:17 +1200

    Add query

--- packages/frontend/src/views/Calendarview/Calendarview.tsx
+++ packages/frontend/src/views/Calendarview/Calendarview.tsx
@@ -1,5 +1,7 @@
 import styles from './Calendarview.module.css';
 import { useAuth } from '../../contexts/AuthContext';
+import { useQuery } from '@apollo/client';
+import { GET_USER_INTERVALS } from '../../apollo/queries';
 
 type CalendarviewProps = {
   prop1: string;
@@ -7,6 +9,20 @@ type CalendarviewProps = {
 
 const Calendarview = ({ prop1 }: CalendarviewProps) => {
   const { setUser } = useAuth();
+
+  const [error, data] = useQuery(GET_USER_INTERVALS, {
+    variables: {
+      userIntervalInput: {
+        intervals: [
+          {
+            start: new Date(),
+            end: new Date(),
+          },
+        ],
+      },
+      availabilityIds: [availabilityIds],
+    },
+  });
 
   return <div className={styles.calendarView}></div>;
 };
