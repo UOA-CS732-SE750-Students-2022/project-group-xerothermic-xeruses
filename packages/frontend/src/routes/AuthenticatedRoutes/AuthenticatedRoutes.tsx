2014d9939f184a34159a6e35fbf64d22b3ba04b8
beverleysun <beverleysun0205@gmail.com>
2022-05-05 01:26:36 +1200

    Remove non-existent `DashboardView`

--- packages/frontend/src/routes/AuthenticatedRoutes/AuthenticatedRoutes.tsx
+++ packages/frontend/src/routes/AuthenticatedRoutes/AuthenticatedRoutes.tsx
@@ -1,12 +1,11 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import SignIn from '../../auth/SignIn';
-import DashboardView from '../../views/DashboardView';
 
 const AuthenticatedRoutes = () => (
   <Routes>
     <Route path="/" element={<Navigate to="/signin" />} />
     <Route path="/signin" element={<SignIn />} />
-    <Route path="/dashboard" element={<DashboardView />} />
+    <Route path="/dashboard" element={<></>} />
   </Routes>
 );
 
