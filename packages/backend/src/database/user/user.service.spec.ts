52e5812ce044f7113d5a18f76dcd7252b517af6c
Maggie Pedersen <mped822@aucklanduni.ac.nz>
2022-05-05 15:18:48 +1200

    Add all user service tests

--- packages/backend/src/database/user/user.service.spec.ts
+++ packages/backend/src/database/user/user.service.spec.ts
@@ -8,6 +8,7 @@ import { UserDocument, UserSchema, USER_MODEL_NAME } from './user.schema';
 import { UserService } from './user.service';
 import { UserAvailabilityDocument } from './userAvailability.schema';
 import { UserAvailabilityICal } from './userAvailabilityICal.schema';
+import { UserAvailabilityProjectionDocument } from './util/projections.types';
 import { UserDatabaseUtilModule } from './util/userDatabaseUtil.module';
 
 const id = (id: string) => {
@@ -21,7 +22,10 @@ const userDocument: Partial<UserDocument> = {
   firebaseId: 'QwerTY12345Qwerty12345qWErTY',
   flocks: [],
   flockInvites: [],
-  availability: [{ _id: id('Availabil_01'), type: 'ical', uri: 'uri://test' }] as UserAvailabilityDocument[],
+  availability: [
+    { _id: id('Availabil_01'), type: 'ical', uri: 'uri://test' },
+    { _id: id('Availabil_02'), type: 'ical', uri: 'uri://test2' },
+  ] as UserAvailabilityDocument[],
 };
 
 describe(UserService.name, () => {
@@ -99,6 +103,36 @@ describe(UserService.name, () => {
     checkEquality(user, updatedUserDocument);
   });
 
+  it('should find a user availability source', async () => {
+    const user: UserDocument | null = await service.findUserAvailability(
+      userDocument._id!,
+      userDocument.availability![0]._id,
+    );
+
+    checkEquality(user, {
+      _id: userDocument._id,
+      availability: [userDocument.availability![0]],
+    });
+  });
+
+  it('should find all user availability sources', async () => {
+    const availabilityIds = userDocument.availability!.map((availability) => availability._id);
+
+    const userAvailabilities: UserAvailabilityProjectionDocument[] = await service.findManyUserAvailability(
+      availabilityIds,
+    );
+
+    expect(userAvailabilities.length).toBe(userDocument.availability?.length);
+
+    userAvailabilities.forEach((userAvailability, index) => {
+      expect(userAvailability).toEqual({
+        _id: userDocument._id,
+        userId: userDocument._id,
+        availabilityDocument: userDocument.availability![index],
+      });
+    });
+  });
+
   it('should add a new user availability source', async () => {
     const user: UserDocument | null = await service.addUserAvailability(userDocument._id!, [
       { type: 'ical', uri: 'uri://another' },
@@ -109,6 +143,14 @@ describe(UserService.name, () => {
     const newAvailability = user.availability.at(-1) as UserAvailabilityICal;
     expect(newAvailability.type).toEqual('ical');
     expect(newAvailability.uri).toEqual('uri://another');
+  });
+
+  it('should not add a duplicate user availability source', async () => {
+    const user: UserDocument | null = await service.addUserAvailability(userDocument._id!, [
+      { type: 'ical', uri: 'uri://test' },
+    ]);
+
+    expect(user.availability.length).toEqual(userDocument.availability!.length);
   });
 
   it('should delete a user successfully', async () => {
@@ -122,16 +164,16 @@ describe(UserService.name, () => {
       await closeMongoDBConnection();
     }
   });
+
+  const checkEquality = (user: UserDocument | null | undefined, expected: Partial<UserDocument> | null | undefined) => {
+    if (user == null) {
+      // If `user` is nullish then `expected` should match it.
+      expect(user).toEqual(expected);
+      return;
+    }
+
+    const userObj = user.toJSON();
+    delete userObj['__v']; // don't check version
+    expect(userObj).toEqual(expected);
+  };
 });
-
-const checkEquality = (user: UserDocument | null | undefined, expected: Partial<UserDocument> | null | undefined) => {
-  if (user == null) {
-    // If `user` is nullish then `expected` should match it.
-    expect(user).toEqual(expected);
-    return;
-  }
-
-  const userObj = user.toJSON();
-  delete userObj['__v']; // don't check version
-  expect(userObj).toEqual(expected);
-};
