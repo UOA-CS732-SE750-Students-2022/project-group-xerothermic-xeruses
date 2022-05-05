243430bd7a0cf956a0c7daa1d60ee9687eae23a3
alieevee <yshe735@aucklanduni.ac.nz>
2022-05-06 11:37:55 +1200

    Remove unused query

--- packages/frontend/src/apollo/queries.ts
+++ packages/frontend/src/apollo/queries.ts
@@ -1,16 +1,5 @@
 import { gql } from '@apollo/client';
 
 // Add queries in this file so that it doesn't clutter up our component files
-const GET_USER_INTERVALS = gql`
-  query UserIntervals($availabilityIds: [String!]!, $userIntervalInput: UserAvailabilityIntervalInput!) {
-    getUserIntervals(availabilityIds: $availabilityIds, userIntervalInput: $userIntervalInput) {
-      availability {
-        start
-        end
-        available
-      }
-    }
-  }
-`;
 
-export { GET_USER_INTERVALS };
+export {};
