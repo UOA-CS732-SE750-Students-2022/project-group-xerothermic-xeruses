728c6d33414dc65e1636cbd51af618dcc1ff0b93
beverleysun <beverleysun0205@gmail.com>
2022-05-06 16:38:55 +1200

    Fix duplicate users being created

--- packages/frontend/src/auth/SignIn.tsx
+++ packages/frontend/src/auth/SignIn.tsx
@@ -1,51 +1,47 @@
-import { useMutation, useLazyQuery } from '@apollo/client';
+import { useMutation, useLazyQuery, ApolloError } from '@apollo/client';
 import { User } from 'firebase/auth';
-import { useEffect, useCallback } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { GET_CURRENT_USER_NAME, CREATE_NEW_USER } from '../apollo/queries';
 import { useAuth } from '../contexts/AuthContext';
 
 const SignIn: React.FC = () => {
-  const [getCurrentUser, { loading, error, data }] = useLazyQuery(GET_CURRENT_USER_NAME);
   const [createUser] = useMutation(CREATE_NEW_USER);
   const { user } = useAuth();
   const navigate = useNavigate();
 
-  const createFlockerUser = useCallback(
-    async (user: User) => {
-      const name = user.displayName;
-      await createUser({
-        variables: {
-          addUserInput: {
-            name,
-          },
+  const createFlockerUser = async (user: User) => {
+    const name = user.displayName;
+    await createUser({
+      variables: {
+        addUserInput: {
+          name,
         },
-      });
-    },
-    [createUser],
-  );
+      },
+    });
+  };
+
+  const handleError = (err: ApolloError) => {
+    const errorCode = err.graphQLErrors[0].extensions.code;
+    (async () => {
+      if (errorCode === '404' && user) {
+        // User does not exist
+        await createFlockerUser(user);
+        navigate('/dashboard', { replace: true });
+      } else {
+        navigate('/', { replace: true }); // TODO: redirect to an "account could not be created" page
+      }
+    })();
+  };
+
+  const [getCurrentUser] = useLazyQuery(GET_CURRENT_USER_NAME, {
+    onCompleted: () => navigate('/dashboard', { replace: true }),
+    onError: handleError,
+  });
 
   useEffect(() => {
-    getCurrentUser(); // This will trigger `loading` to change
+    getCurrentUser();
   }, [user, getCurrentUser]);
-
-  useEffect(() => {
-    if (loading) return;
-    if (data) navigate('/dashboard', { replace: true }); // User exists, go to dashboard
-
-    if (error) {
-      const errorCode = error.graphQLErrors[0].extensions.code;
-      (async () => {
-        if (errorCode === '404' && user) {
-          // User does not exist
-          await createFlockerUser(user);
-          navigate('/dashboard', { replace: true });
-        } else {
-          navigate('/', { replace: true }); // TODO: redirect to an "account could not be created" page
-        }
-      })();
-    }
-  }, [createFlockerUser, loading, data, error, navigate, user]);
 
   return <></>;
 };
