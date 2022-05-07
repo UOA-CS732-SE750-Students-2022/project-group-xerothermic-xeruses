695d910db2af2f5eabc022dfef1c70f4566cae3e
Beverley Sun <beverleysun0205@gmail.com>
2022-05-07 14:09:00 +1200

    Include username in sidebar (#129)

--- packages/frontend/src/layouts/SidebarLayout/SidebarLayout.tsx
+++ packages/frontend/src/layouts/SidebarLayout/SidebarLayout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './SidebarLayout.module.css';
 import Sidebar from '../../components/Sidebar';
+import { useAuth } from '../../contexts/AuthContext';
 
 type SidebarLayoutProps = {
   title?: string;
@@ -8,16 +9,21 @@ type SidebarLayoutProps = {
   bodyContent?: React.ReactNode;
 };
 
-const SidebarLayout: React.FC<SidebarLayoutProps> = ({ title, sidebarContent, bodyContent }) => (
-  <div className={styles.container}>
-    <div className={styles.sidebar}>
-      <Sidebar>{sidebarContent}</Sidebar>
+const SidebarLayout: React.FC<SidebarLayoutProps> = ({ title, sidebarContent, bodyContent }) => {
+  const { user } = useAuth();
+  const username = user?.displayName;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.sidebar}>
+        <Sidebar username={username}>{sidebarContent}</Sidebar>
+      </div>
+      <div className={styles.content}>
+        {<h1 className={styles.title}>{title}</h1>}
+        {bodyContent}
+      </div>
     </div>
-    <div className={styles.content}>
-      {<h1 className={styles.title}>{title}</h1>}
-      {bodyContent}
-    </div>
-  </div>
-);
+  );
+};
 
 export default SidebarLayout;
