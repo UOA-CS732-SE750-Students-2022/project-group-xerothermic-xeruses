d6adb3bbd1203818077e238b8e5ac8363cc5a93a
alieevee <yshe735@aucklanduni.ac.nz>
2022-05-07 18:09:16 +1200

    Add tests

--- packages/frontend/src/components/ParticipantList/ParticipantList.test.tsx
+++ packages/frontend/src/components/ParticipantList/ParticipantList.test.tsx
@@ -1,4 +1,37 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import ParticipantList from './ParticipantList';
 
-test('first test', () => {});
+type Participant = {
+  name: string;
+  id: string;
+};
+
+const participant1: Participant = {
+  name: 'John Smith',
+  id: '1',
+};
+
+const participant2: Participant = {
+  name: 'Jane Doe',
+  id: '2',
+};
+
+it('should render', () => {
+  const { container } = render(<ParticipantList participants={[]} />);
+  expect(container).toBeVisible();
+});
+
+it('should show all participant names', () => {
+  render(<ParticipantList participants={[participant1, participant2]} />);
+  const participantName1 = screen.getByText(/John Smith/i);
+  const participantName2 = screen.getByText(/Jane Doe/i);
+  expect(participantName1).toBeVisible();
+  expect(participantName2).toBeVisible();
+});
+
+it('should have avatars with the first letter of the participant name', () => {
+  render(<ParticipantList participants={[participant1]} />);
+  const participantName1 = screen.getAllByText(/J/i);
+  expect(participantName1).toHaveLength(2);
+});
