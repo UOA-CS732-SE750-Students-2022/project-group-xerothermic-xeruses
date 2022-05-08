7f9deb076e0a2e7d63ed2435a9af542805960f62
beverleysun <beverleysun0205@gmail.com>
2022-05-08 15:57:28 +1200

    Create meeting view

--- packages/frontend/src/routes/AuthenticatedRoutes/AuthenticatedRoutes.tsx
+++ packages/frontend/src/routes/AuthenticatedRoutes/AuthenticatedRoutes.tsx
@@ -1,11 +1,16 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import SignIn from '../../auth/SignIn';
+import DashboardLayout from '../../layouts/DashboardLayout';
+import MeetingsView from '../../views/MeetingsView';
 
 const AuthenticatedRoutes = () => (
   <Routes>
     <Route path="/" element={<Navigate to="/signin" />} />
     <Route path="/signin" element={<SignIn />} />
-    <Route path="/dashboard" element={<></>} />
+    <Route path="/dashboard" element={<DashboardLayout />}>
+      <Route index element={<Navigate to="meetings" />} />
+      <Route path="meetings" element={<MeetingsView />} />
+    </Route>
   </Routes>
 );
 
---
+++ packages/frontend/src/views/MeetingsView/MeetingsView.test.tsx
@@ -0,0 +1,103 @@
+import '@testing-library/jest-dom';
+import MeetingsView from './MeetingsView';
+import { render, screen, act } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
+import { GET_USER_FLOCKS } from '../../apollo';
+import { BrowserRouter } from 'react-router-dom';
+
+const flockResultMock = {
+  request: {
+    query: GET_USER_FLOCKS,
+  },
+  result: {
+    data: {
+      getCurrentUser: {
+        flocks: [
+          {
+            name: 'Meeting 1',
+            flockDays: [{ start: new Date('2022-05-07T00:00:00.000Z'), end: new Date('2022-05-12T00:00:00.000Z') }],
+            users: [{ id: '1' }, { id: '2' }],
+            flockCode: '1',
+          },
+          {
+            name: 'Meeting 2',
+            flockDays: [{ start: new Date('2022-05-11T00:00:00.000Z'), end: new Date('2022-05-19T00:00:00.000Z') }],
+            users: [{ id: '1' }, { id: '2' }, { id: '3' }],
+            flockCode: '2',
+          },
+        ],
+      },
+    },
+  },
+};
+
+const emptyFlockResultMock = {
+  request: {
+    query: GET_USER_FLOCKS,
+  },
+  result: {
+    data: {
+      getCurrentUser: {
+        flocks: [],
+      },
+    },
+  },
+};
+
+const errorMock = {
+  request: {
+    query: GET_USER_FLOCKS,
+  },
+  error: new Error('An error occurred'),
+};
+
+const getComponent = () => (
+  <BrowserRouter>
+    <MeetingsView />
+  </BrowserRouter>
+);
+
+test('should render', async () => {
+  const { container } = render(<MockedProvider>{getComponent()}</MockedProvider>);
+  expect(container).toBeVisible();
+});
+
+test('should show error message when no data is returned', async () => {
+  render(<MockedProvider>{getComponent()}</MockedProvider>);
+  await act(() => new Promise((resolve) => setTimeout(resolve, 0)));
+  const text = screen.queryByText("Sorry, we couldn't get your meetings :(");
+  expect(text).toBeVisible();
+});
+
+test('should show meeting cards', async () => {
+  render(<MockedProvider mocks={[flockResultMock]}>{getComponent()}</MockedProvider>);
+  await act(() => new Promise((resolve) => setTimeout(resolve, 0)));
+
+  const title1 = screen.queryByText('Meeting 1');
+  const participants1 = screen.queryByText('2 participants');
+  const dates1 = screen.queryByText('May 7, 2022 - May 12, 2022');
+  expect(title1).toBeVisible();
+  expect(participants1).toBeVisible();
+  expect(dates1).toBeVisible();
+
+  const title2 = screen.queryByText('Meeting 2');
+  const participants2 = screen.queryByText('3 participants');
+  const dates2 = screen.queryByText('May 11, 2022 - May 19, 2022');
+  expect(title2).toBeVisible();
+  expect(participants2).toBeVisible();
+  expect(dates2).toBeVisible();
+});
+
+test('should show message when user had no flocks', async () => {
+  render(<MockedProvider mocks={[emptyFlockResultMock]}>{getComponent()}</MockedProvider>);
+  await act(() => new Promise((resolve) => setTimeout(resolve, 0)));
+  const text = screen.queryByText('You are not part of any meetings');
+  expect(text).toBeVisible();
+});
+
+test('should show error message when error occurs', async () => {
+  render(<MockedProvider mocks={[errorMock]}>{getComponent()}</MockedProvider>);
+  await act(() => new Promise((resolve) => setTimeout(resolve, 0)));
+  const text = screen.queryByText("Sorry, we couldn't get your meetings :(");
+  expect(text).toBeVisible();
+});
