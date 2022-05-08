16e28b79a4cdac4c2d898c9a1babc3e1eeeb80bf
Beverley Sun <beverleysun0205@gmail.com>
2022-05-08 20:04:57 +1200

    Create MeetingView (#141)
    * Remove title
    
    * Add title layout
    
    * Make content required
    
    * Create dashboard layout
    
    * Refactor test
    
    * Create meeting view
    
    * Remove `useNavigate`
    
    * Remove `useNavigate` again
    
    * Use `it` instead of `test`

--- packages/frontend/src/apollo/queries.ts
+++ packages/frontend/src/apollo/queries.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 // Add queries in this file so that it doesn't clutter up our component files
 
-const GET_CURRENT_USER_NAME = gql`
+export const GET_CURRENT_USER_NAME = gql`
   query Query {
     getCurrentUser {
       name
@@ -10,7 +10,7 @@ const GET_CURRENT_USER_NAME = gql`
   }
 `;
 
-const CREATE_NEW_USER = gql`
+export const CREATE_NEW_USER = gql`
   mutation Mutation($addUserInput: AddUserInput!) {
     addUser(addUserInput: $addUserInput) {
       name
@@ -18,4 +18,20 @@ const CREATE_NEW_USER = gql`
   }
 `;
 
-export { GET_CURRENT_USER_NAME, CREATE_NEW_USER };
+export const GET_USER_FLOCKS = gql`
+  query Flocks {
+    getCurrentUser {
+      flocks {
+        name
+        flockCode
+        flockDays {
+          start
+          end
+        }
+        users {
+          id
+        }
+      }
+    }
+  }
+`;
--- packages/frontend/src/layouts/DashboardLayout/DashboardLayout.test.tsx
+++ packages/frontend/src/layouts/DashboardLayout/DashboardLayout.test.tsx
@@ -1,8 +1,13 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import DashboardLayout from './DashboardLayout';
+import { BrowserRouter } from 'react-router-dom';
 
-test('should render', () => {
-  const { container } = render(<DashboardLayout />);
+it('should render', () => {
+  const { container } = render(
+    <BrowserRouter>
+      <DashboardLayout />
+    </BrowserRouter>,
+  );
   expect(container).toBeVisible();
 });
