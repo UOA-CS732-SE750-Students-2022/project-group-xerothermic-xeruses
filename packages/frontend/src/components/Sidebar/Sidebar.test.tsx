19e3f13ce778897ffa8c9be25529e213b40882f9
Beverley Sun <beverleysun0205@gmail.com>
2022-05-10 22:31:40 +1200

    Enable users to sign out (#170)

--- packages/frontend/src/components/Sidebar/Sidebar.test.tsx
+++ packages/frontend/src/components/Sidebar/Sidebar.test.tsx
@@ -1,32 +1,47 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Sidebar from './Sidebar';
 
 it('should render', () => {
-  const { container } = render(<Sidebar />);
+  const { container } = render(
+    <MemoryRouter>
+      <Sidebar />
+    </MemoryRouter>,
+  );
   expect(container).toBeVisible();
 });
 
 it("should render the user's name if it is supplied", () => {
-  render(<Sidebar username="Goose" />);
+  render(
+    <MemoryRouter>
+      <Sidebar username="Goose" />
+    </MemoryRouter>,
+  );
   const name = screen.queryByText('Goose');
   expect(name).toBeVisible();
 });
 
 it('should handle when username is not supplied', () => {
-  render(<Sidebar />);
+  render(
+    <MemoryRouter>
+      <Sidebar />
+    </MemoryRouter>,
+  );
   const heading = screen.queryByText('Hello!');
   expect(heading).toBeVisible();
 });
 
 it('should render children', () => {
   render(
-    <Sidebar>
-      <div role="children">
-        <p>Paragaph</p>
-        <p>Another paragraph</p>
-      </div>
-    </Sidebar>,
+    <MemoryRouter>
+      <Sidebar>
+        <div role="children">
+          <p>Paragaph</p>
+          <p>Another paragraph</p>
+        </div>
+      </Sidebar>
+    </MemoryRouter>,
   );
 
   const child = screen.queryByRole('children');
