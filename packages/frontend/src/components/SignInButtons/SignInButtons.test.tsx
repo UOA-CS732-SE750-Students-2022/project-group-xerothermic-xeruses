afb954ff87fe89c7f942f57526bb8964b4180cf9
beverleysun <beverleysun0205@gmail.com>
2022-05-10 12:41:57 +1200

    Make sign in buttons into separate component

---
+++ packages/frontend/src/components/SignInButtons/SignInButtons.test.tsx
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import SignInButtons from './SignInButtons';
+import * as firebase from '../../auth/firebase';
+
+beforeAll(() => {});
+
+it('should render', () => {
+  render(<SignInButtons />);
+  expect(screen.getByText('Sign in with Google')).toBeVisible();
+  expect(screen.getByText('or')).toBeVisible();
+  expect(screen.getByText('Continue as Guest')).toBeVisible();
+});
+
+it('should sign in with google', () => {
+  const mock = jest.fn();
+  jest.spyOn(firebase, 'signInWithGoogle').mockImplementation(mock);
+
+  render(<SignInButtons />);
+  const button = screen.getByText('Sign in with Google');
+  fireEvent.click(button);
+  expect(mock).toBeCalledTimes(1);
+});
