1928fec5fbb100074afe021a7359c348b73690e0
Maggie Pedersen <62322685+mped822@users.noreply.github.com>
2022-05-10 14:36:23 +1200

    Add 404 page (#157)
    * Basic mockup
    
    * Add tests
    
    * Linting
    
    * Change to column layout
    
    * Update README
    
    * Add props message
    
    * Remove oopsie
    
    * Indicate required props
    
    * Fix nits
    
    * Make button white
    
    Co-authored-by: Beverley Sun <beverleysun0205@gmail.com>

---
+++ packages/frontend/src/views/NotFoundView/NotFoundView.test.tsx
@@ -0,0 +1,50 @@
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+import DashboardLayout from '../../layouts/DashboardLayout';
+import HomeView from '../HomeView';
+import NotFoundView from './NotFoundView';
+
+const renderViewsWithMemory = (goBackLink: string) => {
+  render(
+    <MemoryRouter initialEntries={['/bad/route']}>
+      <HomeView />
+      <DashboardLayout />
+      <NotFoundView goBackLink={goBackLink} />
+    </MemoryRouter>,
+  );
+};
+
+it('should render', () => {
+  const { container } = render(
+    <BrowserRouter>
+      <NotFoundView goBackLink="/" />
+    </BrowserRouter>,
+  );
+
+  expect(container).toBeVisible();
+  expect(screen.getByText('404')).toBeVisible();
+  expect(screen.getByText('Go Back')).toBeVisible();
+  expect(screen.getByText('The goose stole the page you were looking for!')).toBeVisible();
+});
+
+it('should take the user back to the home page when they are unauthenticated', () => {
+  renderViewsWithMemory('/');
+
+  expect(screen.getByText('404')).toBeVisible();
+  fireEvent.click(screen.getByText('Go Back'));
+
+  // Ensure we go back to the home page
+  expect(screen.getByText('Sign in with Google')).toBeInTheDocument();
+  expect(screen.getByText('Continue as Guest')).toBeInTheDocument();
+});
+
+it('should take the user back to the dashboard when they are authenticated', () => {
+  renderViewsWithMemory('/dashboard');
+
+  expect(screen.getByText('404')).toBeVisible();
+  fireEvent.click(screen.getByText('Go Back'));
+
+  // Ensure we go back to the dashboard
+  expect(screen.getByText('My Meetings')).toBeInTheDocument();
+});
