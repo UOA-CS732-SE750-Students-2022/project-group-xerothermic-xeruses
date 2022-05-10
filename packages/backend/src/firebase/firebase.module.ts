24ef44e6aba4b0a8f2f657c35cfb7bb34e8bf744
Matt Moran <mattmoran2000@gmail.com>
2022-05-10 18:08:52 +1200

    Partial backend support for Google Calendar imports (#160)
    * Add Google OAuth2 config
    * Add `FirebaseService` with `getUserFromGoogleIdToken`
    * Add support for handling Google OAuth2 tokens
    * Add multiple googlecalendars to database

--- packages/backend/src/firebase/firebase.module.ts
+++ packages/backend/src/firebase/firebase.module.ts
@@ -3,11 +3,19 @@ import { FirebaseConfigModule } from '~/config/firebaseConfig.module';
 import { UserDatabaseModule } from '~/database/user/user.module';
 import { FirebaseAuthGuard } from './firebase-auth.guard';
 import { FirebaseAuthStrategy } from './firebase-auth.strategy';
+import { FirebaseService } from './firebase.service';
 import { ValidateUserFirebaseAuthGuard } from './validate-user-firebase-auth.guard';
 import { ValidateUserFirebaseAuthStrategy } from './validate-user-firebase-auth.strategy';
 
 @Module({
   imports: [FirebaseConfigModule, UserDatabaseModule],
-  providers: [FirebaseAuthStrategy, ValidateUserFirebaseAuthStrategy, FirebaseAuthGuard, ValidateUserFirebaseAuthGuard],
+  providers: [
+    FirebaseAuthStrategy,
+    ValidateUserFirebaseAuthStrategy,
+    FirebaseAuthGuard,
+    ValidateUserFirebaseAuthGuard,
+    FirebaseService,
+  ],
+  exports: [FirebaseService],
 })
 export class FirebaseModule {}
