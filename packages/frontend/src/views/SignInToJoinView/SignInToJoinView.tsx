51d4b1d011fb82619a55e9c9d7fb354420aa37af
beverleysun <beverleysun0205@gmail.com>
2022-05-10 13:41:58 +1200

    Add logo

--- packages/frontend/src/views/SignInToJoinView/SignInToJoinView.tsx
+++ packages/frontend/src/views/SignInToJoinView/SignInToJoinView.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Logo from '../../components/Logo';
 import SignInButtons from '../../components/SignInButtons';
 import styles from './SignInToJoinView.module.css';
 
@@ -7,6 +8,9 @@ const SignInToJoin: React.FC = () => (
     <img className={`${styles.goose}`} src={`${process.env.PUBLIC_URL}/assets/images/flocker-goose.png`} alt="logo" />
     <h1 className={styles.title}>Please sign in to join the meeting</h1>
     <SignInButtons />
+    <div className={styles.logo}>
+      <Logo size="footer" colored={false} />
+    </div>
   </div>
 );
 
