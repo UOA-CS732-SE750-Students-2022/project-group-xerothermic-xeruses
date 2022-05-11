7a6cbf7e380e54aa32794bd1d929c6fdd45bf41d
alieevee <yshe735@aucklanduni.ac.nz>
2022-05-11 12:15:36 +1200

    Add route

--- packages/frontend/src/routes/AuthenticatedRoutes/AuthenticatedRoutes.tsx
+++ packages/frontend/src/routes/AuthenticatedRoutes/AuthenticatedRoutes.tsx
@@ -2,6 +2,8 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import SignIn from '../../auth/SignIn';
 import SignOut from '../../auth/SignOut';
 import DashboardLayout from '../../layouts/DashboardLayout';
+import CalendarView from '../../views/CalendarView';
+import Calendarview from '../../views/CalendarView';
 import CreateMeetingView from '../../views/CreateMeetingView';
 import MeetingsView from '../../views/MeetingsView';
 import NotFoundView from '../../views/NotFoundView';
@@ -14,6 +16,10 @@ const AuthenticatedRoutes = () => (
       <Route index element={<Navigate to="meetings" />} />
       <Route path="meetings" element={<MeetingsView />} />
       <Route path="create-meeting" element={<CreateMeetingView />} />
+    </Route>
+    <Route path="/:flockCode">
+      <Route index element={<Navigate to="calendar" />} />
+      <Route path="calendar" element={<CalendarView />} />
     </Route>
     <Route path="/signout" element={<SignOut />} />
     <Route path="*" element={<NotFoundView goBackLink="/dashboard" />} />
