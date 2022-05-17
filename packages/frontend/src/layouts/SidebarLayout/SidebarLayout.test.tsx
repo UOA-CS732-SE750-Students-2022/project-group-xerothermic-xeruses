6ff1e6ae5056a31626514a608b2b34655fc96bf8
alieevee <yshe735@aucklanduni.ac.nz>
2022-05-17 22:12:41 +1200

    Remove duplicate import statement

--- packages/frontend/src/layouts/SidebarLayout/SidebarLayout.test.tsx
+++ packages/frontend/src/layouts/SidebarLayout/SidebarLayout.test.tsx
@@ -3,7 +3,6 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import SidebarLayout from './SidebarLayout';
-import { MemoryRouter } from 'react-router-dom';
 
 it('should render', () => {
   render(
