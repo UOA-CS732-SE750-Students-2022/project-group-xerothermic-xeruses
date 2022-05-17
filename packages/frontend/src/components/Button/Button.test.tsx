49e6607267c68e6555a9f599d34dd756839c6a0c
Beverley Sun <beverleysun0205@gmail.com>
2022-05-17 13:00:06 +1200

    Use `it` instead of `test` (#184)

--- packages/frontend/src/components/Button/Button.test.tsx
+++ packages/frontend/src/components/Button/Button.test.tsx
@@ -2,12 +2,12 @@ import '@testing-library/jest-dom';
 import { fireEvent, render, screen } from '@testing-library/react';
 import Button from './Button';
 
-test('should exist', () => {
+it('should render', () => {
   const { container } = render(<Button />);
   expect(container).toBeVisible();
 });
 
-test('applies correct color class names', () => {
+it('should apply correct color class names', () => {
   render(<Button color="primary">Text</Button>);
   render(<Button color="white">Text</Button>);
   render(<Button color="black">Text</Button>);
@@ -18,7 +18,7 @@ test('applies correct color class names', () => {
   expect(buttons[2]).toHaveClass('black');
 });
 
-test('applies correct variant class names', () => {
+it('should apply correct variant class names', () => {
   render(<Button variant="filled">Text</Button>);
   render(<Button variant="outlined">Text</Button>);
   const buttons = screen.getAllByText('Text');
@@ -27,7 +27,7 @@ test('applies correct variant class names', () => {
   expect(buttons[1]).toHaveClass('outlined');
 });
 
-test('calls onClick prop when clicked', () => {
+it('should call onClick prop when clicked', () => {
   const handleClick = jest.fn();
   render(<Button onClick={handleClick}>Text</Button>);
   const button = screen.getByText('Text');
