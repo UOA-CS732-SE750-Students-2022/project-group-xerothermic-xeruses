858ab5fd3a890af41e545d66ff92b34c2737c84d
alieevee <yshe735@aucklanduni.ac.nz>
2022-05-17 19:11:24 +1200

    Move all preprocessing to view from components

--- packages/frontend/src/views/CalendarView/CalendarView.tsx
+++ packages/frontend/src/views/CalendarView/CalendarView.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@apollo/client';
-import React from 'react';
+import React, { useState } from 'react';
 import Timematcher from '../../components/Timematcher';
 import SidebarLayout from '../../layouts/SidebarLayout';
 import TitleLayout from '../../layouts/TitleLayout';
@@ -21,22 +21,113 @@ import ParticipantList from '../../components/ParticipantList';
 import Line from '../../components/Line';
 import { UserAvailabilityPartialDTO, UserIntervalInputDTO } from '@flocker/api-types';
 import CalendarList from '../../components/CalendarList';
-import Button from '../../components/Button';
-
-type FlockParams = {
-  flockCode: string;
-};
 
 type Availability = {
   start: Date;
   end: Date;
   available: boolean;
 };
+
+type Calendar = {
+  name: string;
+  id: string;
+  enabled: boolean;
+  onEnabledChanged: (enabled: boolean) => void;
+};
+
 const FIFTEEN_MINUTES = 15 * 60000;
 
 let availabilityIds: string[] = [];
 
-const Flock: React.FC = () => {
+export const useEnabled = (calendars: Calendar[]) => {
+  const [availabilityIdList, setAvailabilityIdList] = useState(availabilityIds);
+  calendars.forEach((calendar) => {
+    if (calendar.enabled === false) {
+      if (availabilityIds.includes(calendar.id)) {
+        availabilityIds = availabilityIds.filter((id) => id !== calendar.id);
+      }
+    } else {
+      if (!availabilityIds.includes(calendar.id)) {
+        availabilityIds.push(calendar.id);
+      }
+    }
+  });
+  const changeAvailabilityIdList = () => setAvailabilityIdList(availabilityIds);
+  return [availabilityIdList, changeAvailabilityIdList] as const;
+};
+
+type FlockProps = {
+  flockName: string;
+  datesPicked: Date[];
+  timeRange: [Date, Date];
+  userAvailability: Availability[];
+  othersAvailability: Availability[];
+};
+
+const Flock: React.FC<FlockProps> = ({ flockName, datesPicked, timeRange, userAvailability, othersAvailability }) => {
+  return (
+    <TitleLayout
+      title={flockName}
+      content={
+        <>
+          <div className={styles.timeMatcher}>
+            <Timematcher
+              datesPicked={datesPicked}
+              timeRange={timeRange}
+              userAvailability={userAvailability}
+              othersAvailability={othersAvailability}
+            />
+            <Legend />
+          </div>
+        </>
+      }
+    />
+  );
+};
+
+const Legend: React.FC = () => {
+  return (
+    <div>
+      <h2>Legend</h2>
+      <div className={styles.legendKeys}>
+        <div className={styles.circleBothAvailable} />
+        <h3>All available</h3>
+        <div className={styles.circleUserAvailable} />
+        <h3>You're available</h3>
+        <div className={styles.circleFlockAvailable} />
+        <h3>Others available</h3>
+      </div>
+    </div>
+  );
+};
+
+type Participant = {
+  id: string;
+  name: string;
+};
+
+type CalendarSidebarProps = {
+  participantList: Participant[];
+  calendarList: Calendar[];
+};
+
+const CalendarViewSidebar: React.FC<CalendarSidebarProps> = ({ participantList, calendarList }) => {
+  return (
+    <div>
+      <h1 className={styles.sidebarHeadings}>Participants</h1>
+      <ParticipantList participants={participantList} />
+      <Line />
+      <h1 className={styles.sidebarHeadings}>Calendars</h1>
+      <CalendarList calendars={calendarList} onUpdate={(calendars) => {}} />
+    </div>
+  );
+};
+
+type FlockParams = {
+  flockCode: string;
+};
+
+const CalendarView: React.FC = () => {
   const { flockCode } = useParams<FlockParams>();
   const flock = useQuery<GetCurrentFlockResult>(GET_USER_FLOCK, {
     variables: { flockCode: flockCode },
@@ -118,59 +209,18 @@ const Flock: React.FC = () => {
     });
   }
 
-  if (flock.loading || userIntervals.loading) return <CircularProgress />;
-  if (flock.error) return errorMessage;
+  //from calendarsidebar
 
-  return (
-    <TitleLayout
-      title={flockName}
-      content={
-        <>
-          <div className={styles.timeMatcher}>
-            <Timematcher
-              datesPicked={datesPicked}
-              timeRange={timeRange}
-              userAvailability={userAvailabilities}
-              othersAvailability={flockAvailabilities}
-            />
-            <Legend />
-          </div>
-        </>
-      }
-    />
-  );
-};
-
-const Legend: React.FC = () => {
-  return (
-    <div>
-      <h2>Legend</h2>
-      <div className={styles.legendKeys}>
-        <div className={styles.circleBothAvailable} />
-        <h3>All available</h3>
-        <div className={styles.circleUserAvailable} />
-        <h3>You're available</h3>
-        <div className={styles.circleFlockAvailable} />
-        <h3>Others available</h3>
-      </div>
-    </div>
-  );
-};
-
-const CalendarViewSidebar: React.FC = () => {
-  const { flockCode } = useParams<FlockParams>();
   const participants = useQuery<GetCurrentFlockResult>(GET_FLOCK_PARTICIPANTS, {
     variables: { flockCode: flockCode },
   });
   const calendars = useQuery<GetCurrentUserResult>(GET_USER_CALENDARS);
-  const errorMessage = <>Sorry, we couldn't get the participants of the meeting :(</>;
+
   if (participants.loading || calendars.loading) return <CircularProgress />;
   if (participants.error || calendars.error) return errorMessage;
 
-  type Participant = {
-    id: string;
-    name: string;
-  };
+  if (flock.loading || userIntervals.loading) return <CircularProgress />;
+  if (flock.error) return errorMessage;
 
   let participantList: Participant[] = [];
 
@@ -210,16 +260,19 @@ const CalendarViewSidebar: React.FC = () => {
   }
 
   return (
-    <div>
-      <h1 className={styles.sidebarHeadings}>Participants</h1>
-      <ParticipantList participants={participantList} />
-      <Line />
-      <h1 className={styles.sidebarHeadings}>Calendars</h1>
-      <CalendarList calendars={calendarList} onUpdate={() => {}} />
-    </div>
+    <SidebarLayout
+      sidebarContent={<CalendarViewSidebar participantList={participantList} calendarList={calendarList} />}
+      bodyContent={
+        <Flock
+          flockName={flockName}
+          datesPicked={datesPicked}
+          timeRange={timeRange}
+          userAvailability={userAvailabilities}
+          othersAvailability={flockAvailabilities}
+        />
+      }
+    />
   );
 };
-
-const CalendarView: React.FC = () => <SidebarLayout sidebarContent={<CalendarViewSidebar />} bodyContent={<Flock />} />;
 
 export default CalendarView;
