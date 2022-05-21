1e91e823f78df302cf6837cd5803e17b065894e2
Beverley Sun <beverleysun0205@gmail.com>
2022-05-21 16:32:55 +1200

    Add text to show if list is empty (#229)

--- packages/frontend/src/components/ParticipantList/ParticipantList.tsx
+++ packages/frontend/src/components/ParticipantList/ParticipantList.tsx
@@ -6,7 +6,7 @@ import ListItemText from '@mui/material/ListItemText';
 import Avatar from '@mui/material/Avatar';
 
 type ParticipantListProps = {
-  participants: Participant[] | Participant;
+  participants: Participant[];
 };
 
 type Participant = {
@@ -18,19 +18,24 @@ const getFirstLetter = (participant: string) => {
   return participant.charAt(0).toUpperCase();
 };
 
-const ParticipantList: React.FC<ParticipantListProps> = ({ participants }) => (
-  <div className={styles.participantList}>
-    <List className={styles.listContent}>
-      {Array.from(participants as Participant[]).map((participant) => (
-        <ListItem className={styles.listItem} key={participant.id}>
-          <Avatar className={styles.avatar}>
-            <p className={styles.avatarText}>{getFirstLetter(participant.name)}</p>
-          </Avatar>
-          <ListItemText primary={<p className={styles.name}>{participant.name}</p>} />
-        </ListItem>
-      ))}
-    </List>
-  </div>
-);
+const ParticipantList: React.FC<ParticipantListProps> = ({ participants }) => {
+  if (!participants.length) {
+    return <p className={styles.noParticipants}>Join to be the first in this flock!</p>;
+  }
+  return (
+    <div className={styles.participantList}>
+      <List className={styles.listContent}>
+        {participants.map((participant) => (
+          <ListItem className={styles.listItem} key={participant.id}>
+            <Avatar className={styles.avatar}>
+              <p className={styles.avatarText}>{getFirstLetter(participant.name)}</p>
+            </Avatar>
+            <ListItemText primary={<p className={styles.name}>{participant.name}</p>} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+};
 
 export default ParticipantList;
